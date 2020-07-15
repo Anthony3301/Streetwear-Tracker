e1f61da78cb2004b867e6916acea10f95b1922f2
Anthony3301 <anthonypirvuti@gmail.com>
2020-07-15 18:29:09 -0400

    removed result number functionality

--- stockX/price_finder.py
+++ stockX/price_finder.py
@@ -26,14 +26,6 @@ try:
     main = WebDriverWait(browser,30).until(
         EC.presence_of_element_located((By.ID,"products-container"))
     )
-
-    #checks if the number of results are present
-    isResults = len(browser.find_elements_by_xpath("//*[@id='products-container']/div[1]/div/b")) > 0
-    print(isResults)
-
-    result = browser.find_element_by_xpath("//*[@id='products-container']/div[1]/div/b").text
-    print(result)
-
-    
-except:
+   
+finally:
     browser.quit()
